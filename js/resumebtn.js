9a8189b05523b1e1176751468d37d5b47107c2ad
FreyAnalytics <frey.ilya.32@gmail.com>
2023-10-05 16:37:51 +0200

    minor

--- js/resumebtn.js
+++ js/resumebtn.js
@@ -4,7 +4,7 @@ const openPdfButton = document.getElementById('openPdfButton');
 // Add a click event listener to the button
 openPdfButton.addEventListener('click', function() {
     // Specify the path to the local PDF file on your computer
-    const pdfPath = 'frey_resume.pdf';
+    const pdfPath = 'cv_EliasFrey.pdf';
 
     // Open the PDF in a new tab
     window.open(pdfPath, '_blank');
